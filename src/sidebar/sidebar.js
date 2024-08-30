e6cd38cf7dd7e3c427fe65c50e5180098c277c8a
balrazhanmanthugari <balu333hanmanthu@gmail.com>
2024-08-30 09:28:09 -0700

    style changes

--- src/sidebar/sidebar.js
+++ src/sidebar/sidebar.js
@@ -1,19 +1,11 @@
-import React, { useState } from "react";
+import React from "react";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
-import * as FaIcons from "react-icons/fa";
 import * as AiIcons from "react-icons/ai";
 import { SidebarData } from "./SidebarData";
 import SubMenu from "./SubMenu";
 import { IconContext } from "react-icons/lib";
 
-const Nav = styled.div`
-    background: #1a3986;
-    height: 80px;
-    display: flex;
-    justify-content: flex-start;
-    align-items: center;
-`;
 
 const NavIcon = styled(Link)`
     margin-left: 2rem;
@@ -41,31 +33,14 @@ const SidebarWrap = styled.div`
     width: 100%;
 `;
 
-const Sidebar = () => {
-    const [sidebar, setSidebar] = useState(true);
-
-    const showSidebar = () => setSidebar(!sidebar);
-
+const Sidebar = (props) => {
+    const showSidebar = () => {
+        props.props.show();
+    }
     return (
         <>
             <IconContext.Provider value={{ color: "#fff" }}>
-                <Nav>
-                    <NavIcon to="#">
-                        <FaIcons.FaBars
-                            onClick={showSidebar}
-                        />
-                    </NavIcon>
-                    <h1
-                        style={{
-                            textAlign: "center",
-                            marginLeft: "200px",
-                            color: "#fff",
-                        }}
-                    >
-                        Balraj
-                    </h1>
-                </Nav>
-                <SidebarNav sidebar={sidebar}>
+                <SidebarNav sidebar={props.props.sidebar}>
                     <SidebarWrap>
                         <NavIcon to="#">
                             <AiIcons.AiOutlineClose
