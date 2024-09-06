c7889ff49e893f68a0bf6e3e46fe5f735a913505
balrazhanmanthugari <balu333hanmanthu@gmail.com>
2024-09-06 16:17:03 -0700

    added user

--- src/my/table/index.js
+++ src/my/table/index.js
@@ -1,50 +1,8 @@
 
 
 
-
-//let  data = {sno: Number, name: String, surName: String, contact: Number, address: String }
+import React, { Component } from 'react';
 import axios from "axios";
-import {useState} from "react";
-
-let data1 = [
-    {
-        sno:1,
-        name: "balraj",
-        surName: "H",
-        contact: 234,
-        address: "12"
-    },
-    {
-        sno:2,
-        name: "balu",
-        surName: "H",
-        contact: 234,
-        address: "12"
-    },
-    {
-        sno:3,
-        name: "balz",
-        surName: "H",
-        contact: 234,
-        address: "12"
-    },
-    {
-        sno:4,
-        name: "bal5225",
-        surName: "H",
-        contact: 234,
-        address: "12"
-    },
-    {
-        sno:5,
-        name: "balrajH",
-        surName: "H",
-        contact: 234,
-        address: "12"
-    },
-
-]
-
 
 const printData = (item) => {
     return (
@@ -54,73 +12,81 @@ const printData = (item) => {
                 <table>
                     <tr>
                         <td> Name </td>
-                        <td>{item.name}</td>
+                        <td>{item.firstName}</td>
                     </tr>
                     <tr>
                         <td> SurName </td>
-                        <td>{item.surName}</td>
+                        <td>{item.lastName}</td>
                     </tr>
                     <tr>
                         <td> Contact </td>
-                        <td>{item.contact}</td>
+                        <td>{item.phone}</td>
                     </tr>
                     <tr>
-                        <td> Address </td>
-                        <td>{item.address}</td>
+                        <td> Gender </td>
+                        <td>{item.gender}</td>
                     </tr>
                 </table>
             </div>
     );
 }
 
-function TableData() {
-    const [selectedData, setSelectedData] = useState(undefined);
-    const clickedData = (item) => {
-        setSelectedData(item)
+// eslint-disable-next-line no-undef
+class TableData extends Component {
+    constructor(props) {
+        super(props);
+        this.state ={
+            selectedData : undefined,
+            urlData : []
+        };
+        this.getCustomersData();
     }
-    const getCustomersData = () => {
+
+    clickedData = (item) => {
+        this.setState({selectedData: item})
+    }
+    getCustomersData = () => {
         axios
-            .get("http://192.168.0.22:9090/test/questions",
-                {headers: {
-                        Accept: "applicaiton/json",
-                        "Access-Control-Allow-Origin": "*",
-                        "Content-Type": "application/json",
-                    }})
-            .then(data => console.log(data.data))
+            .get("/user/all")
+            .then(data => {
+                this.setState({urlData: data.data});
+            })
             .catch(error => console.log(error));
     };
-    return(
-        <div>
-            {printData(selectedData)}
-            <button onClick={getCustomersData}> Candidates</button>
-            <h5>All The Candidates </h5>
-            <table>
-                <thead>
-                <tr>
-                    <th>Sno</th>
-                    <th>Name</th>
-                    <th>Sur-Name</th>
-                    <th>Contact</th>
-                    <th>Address</th>
-                </tr>
-                </thead>
-                {data1.map((item, index) =>
-                {
-                    return (
-                    <tbody>
-                      <tr key={index} onClick={ () => {clickedData(item)}}>
-                          <td>{item.sno}</td>
-                          <td>{item.name}</td>
-                          <td>{item.surName}</td>
-                          <td>{item.contact}</td>
-                          <td>{item.address}</td>
-                      </tr>
-                    </tbody>
-                    );
-                })}
-            </table>
-        </div>
-    );
+    render() {
+        return (
+            <div>
+                {printData(this.state.selectedData)}
+                {this.state.urlData.length > 0 ?
+                    <table>
+                        <thead>
+                        <tr>
+                            <th>Sno</th>
+                            <th>Name</th>
+                            <th>Sur-Name</th>
+                            <th>Gender</th>
+                            <th>Contact</th>
+                        </tr>
+                        </thead>
+                        {this.state.urlData.map((item, index) =>
+                        {
+                            return (
+                                <tbody>
+                                <tr key={index} onClick={()=>this.clickedData(item)}>
+                                    <td>{item.id}</td>
+                                    <td>{item.firstName}</td>
+                                    <td>{item.lastName}</td>
+                                    <td>{item.gender}</td>
+                                    <td>{item.phone}</td>
+                                </tr>
+                                </tbody>
+                            );
+                        })}
+                    </table> :null
+                }
+            </div>
+        );
+    }
 }
 
 
